db5f759f406fa07c432fc8562b70597404443539
Parker Zink <parker.zink@gmail.com>
2015-05-06 08:51:43 -0700

    Created new function
    Created a user id function from milestone 10

--- index.php
+++ index.php
@@ -22,7 +22,13 @@ function connectToInstagram($url){
     curl_close($ch);
     return $result;
 }
-
+//function to get userID becuase username does not allow us to get pictures!
+function getUserID($userName){
+    $url = "http://api.instagram.com/vl/users/search?q=".$userName."&client_id=".clientID;
+    $instagramInfo = connectToInstagram($url);
+    $results = json_decode($instagramInfo, true);
+    echo $results['data']['0']['id'];
+}
 if(isset($_GET['code'])){
     $code = ($_GET['code']);
     $url = 'https://api.instagram.com/oauth/access_token';
