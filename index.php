ccba9c724b484350f613b8bf4542c31df879daf2
Parker Zink <parker.zink@gmail.com>
2015-05-15 09:17:04 -0700

    Error fixes
    Made some minor error fixes for the API

--- index.php
+++ index.php
@@ -60,18 +60,22 @@ function connectToInstagram($url){
 }
 //function to get userID becuase username does not allow us to get pictures!
 function getUserID($userName){
-    $url = "https://api.instagram.com/vl/users/search?q=".$userName."&client_id=".clientID; //to get ID
+    $url = "https://api.instagram.com/v1/users/search?q=".$userName."&client_id=".clientID; //to get ID
     $instagramInfo = connectToInstagram($url);//connecting to Instagram
     $results = json_decode($instagramInfo, true);//creating a local variable to decode json information
-    return $results['data']['0']['id'];//echoing out userID
+    
+    //print_r($results);
+    
+    return $results['data'][0]['id'];//echoing out userID
 }
 //function to print out images onto screen
 
 function printImages($userID){
-    $url = "https://api.instagram.com/vl/users/".$userID."/media/recent?client_id=".clientID."&count=5";
+    $url = 'https://api.instagram.com/v1/users/'.$userID.'/media/recent?client_id='.clientID.'&count=5';
     $instagramInfo = connectToInstagram($url);
     $results = json_decode($instagramInfo, true);
     //Parse through the information one by one.
+    print_r($results);
     foreach ($results['data'] as $items){
     $image_url = $items['images']['low_resolution']['url'];
         
@@ -81,9 +85,9 @@ function printImages($userID){
     }
 }
     function savePictures($image_url){
-        echo $image_url . "<br>";
+       return $image_url . "<br>";
         $filename = basename($image_url); //the filename is what we are storing. basename is the PHP built in method that we are using to store $image_url
-        echo $filename . "<br>";
+        return $filename . "<br>";
         
         $destination = ImageDirectory . $filename; 
         file_put_contents($destination, file_get_contents($image_url)); //goes and grabs an image file and stores it into out server
@@ -92,7 +96,7 @@ function printImages($userID){
     
 
 if(isset($_GET['code'])){
-    $code = ($_GET['code']);
+    $code = $_GET['code'];
     $url = 'https://api.instagram.com/oauth/access_token';
     $access_token_settings = array('client_id' => clientID,
                                     'client_secret' => clientSecret,
@@ -170,7 +174,7 @@ renderTime();
     <div id="FinalAnswer"><bgsound id="sound"></div>
 <!-- Creating a login for people to go and give approval for our web app to access their Instagram Account after getting approval we are now -->
 <ul>
-    <font size="92"><a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code" onmouseover="playclip();" onmouseout="stopclip();">Login</a></font>
+    <font size="92"><a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code" onmouseover="playclip();" onclick="playclip()" onmouseout="stopclip();">Login</a></font>
 <br>
 <font size="92"><a href="JavaScript:window.close()" onmouseover="playclip();" onmouseout="stopclip();">Quit</a></font></center>
 
